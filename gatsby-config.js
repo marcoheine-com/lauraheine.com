3e2b558c8b99280049338f9e6d8ea222e990273a
Marco Kühbauch <marcokuehbauch@gmail.com>
2021-12-21 11:20:40 +0100

    configure sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,10 +11,15 @@ module.exports = {
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        exclude: ['/preview'],
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
