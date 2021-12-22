24b1642d871d29fc123605ccc436fd428d29c318
Marco Kühbauch <marcokuehbauch@gmail.com>
2021-12-22 08:30:48 +0100

    update packages, handle preview 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { withPrismicUnpublishedPreview } from 'gatsby-plugin-prismic-previews'
 
 const NotFoundPage = () => {
   return (
@@ -27,4 +28,4 @@ const NotFoundPage = () => {
   )
 }
 
-export default NotFoundPage
+export default withPrismicUnpublishedPreview(NotFoundPage)
